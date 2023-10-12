2ac677f3563e45daed0a74fe9148cdd470c6c72e
Szymon Duda <szymon.duda@sofomo.com>
2023-10-12 13:13:44 +0200

    perf(bundlesize): remove client side references to mathjs

--- src/features/client/Game/useGame.ts
+++ src/features/client/Game/useGame.ts
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { INPUT_LENGTH, isDigitValid } from "@/helpers";
 import { Hint, Message, RiddleResponse } from "./types";
-import { evaluateAnswer } from "@/app/riddle";
 
 const defaultInput = " ".repeat(INPUT_LENGTH);
 const defaultMessage = {
@@ -15,72 +14,6 @@ export const useGame = (riddle: number, id: number) => {
   const [hintses, setHintses] = useState<Hint[][]>([]);
   const [input, setInput] = useState(defaultInput);
   const [cursor, setCursor] = useState(0);
-
-  const submit = async () => {
-    if ([...input].filter((char) => char !== " ").length < INPUT_LENGTH) {
-      setMessage({
-        text: "Row must be complete.",
-        className: "text-red-400",
-      });
-      return;
-    }
-
-    if (evaluateAnswer(input) !== riddle) {
-      setMessage({
-        text: `Expression value must equal ${riddle}.`,
-        className: "text-red-400",
-      });
-      return;
-    }
-
-    setMessage({ text: "Let me check that answer..." });
-    const res = await fetch(`/riddle/${encodeURIComponent(input)}`, {
-      method: "POST",
-    });
-
-    const { solved, message, hints }: RiddleResponse = await res.json();
-    setMessage({
-      text: message ?? "You're doing great!",
-      className: message ? "text-red-400" : "",
-    });
-
-    if (res.status === 200) {
-      setCursor(0);
-      const newHintses = hints ? [...hintses, hints] : hintses;
-
-      if (solved) {
-        setSolved(solved);
-        setMessage({ text: " " });
-      } else if (hints) {
-        if (hintses.length < 6) {
-          setHintses(newHintses);
-          setInput("");
-          setGameover(hintses.length === 5);
-        }
-      }
-
-      localStorage.setItem(
-        "mathler",
-        JSON.stringify({
-          riddleId: id,
-          hintses: newHintses,
-          input: solved ? input : " ".repeat(INPUT_LENGTH),
-          solved,
-          gameover: hints && hintses.length === 5,
-        })
-      );
-    }
-  };
-
-  const clearAnswer = () => {
-    localStorage.removeItem("mathler");
-    setInput(defaultInput);
-    setHintses([]);
-    setMessage(defaultMessage);
-    setGameover(false);
-    setSolved(false);
-    setCursor(0);
-  };
 
   // load stored game info
   useEffect(() => {
@@ -146,6 +79,64 @@ export const useGame = (riddle: number, id: number) => {
     }
     return () => removeEventListener("keydown", handleKeyboard);
   }, [solved, cursor, input]);
+
+  const clearAnswer = () => {
+    localStorage.removeItem("mathler");
+    setInput(defaultInput);
+    setHintses([]);
+    setMessage(defaultMessage);
+    setGameover(false);
+    setSolved(false);
+    setCursor(0);
+  };
+
+  const submit = async () => {
+    if ([...input].filter((char) => char !== " ").length < INPUT_LENGTH) {
+      setMessage({
+        text: "Row must be complete.",
+        className: "text-red-400",
+      });
+      return;
+    }
+
+    setMessage({ text: "Let me check that answer..." });
+    const res = await fetch(`/riddle/${encodeURIComponent(input)}`, {
+      method: "POST",
+    });
+
+    const { solved, message, hints }: RiddleResponse = await res.json();
+    setMessage({
+      text: message ?? "You're doing great!",
+      className: message ? "text-red-400" : "",
+    });
+
+    if (res.status === 200) {
+      setCursor(0);
+      const newHintses = hints ? [...hintses, hints] : hintses;
+
+      if (solved) {
+        setSolved(solved);
+        setMessage({ text: " " });
+      } else if (hints) {
+        if (hintses.length < 6) {
+          setHintses(newHintses);
+          setInput("");
+          setGameover(hintses.length === 5);
+        }
+      }
+
+      localStorage.setItem(
+        "mathler",
+        JSON.stringify({
+          riddleId: id,
+          hintses: newHintses,
+          input: solved ? input : defaultInput,
+          solved,
+          gameover: hints && hintses.length === 5,
+        })
+      );
+    }
+  };
 
   return {
     solved,
